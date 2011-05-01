34fd45b310a23b30282f22b5f6831152aaebc30a
Geoffroy Couprie <geo.couprie@gmail.com>
2011-05-01 16:29:55 +0200

    add parameters

--- app/models/certificate.rb
+++ app/models/certificate.rb
@@ -1,2 +1,24 @@
 class Certificate < ActiveRecord::Base
+
+def country
+end
+
+def state
+end
+
+def city
+end
+
+def organization_name
+end
+
+def organizational_unit_name
+end
+
+def common_name
+end
+
+def email_address
+end
+
 end
