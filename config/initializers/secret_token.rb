904f21ee2e36689ab20d2cc27e380a3a2b303ef9
Geoffroy Couprie <geo.couprie@gmail.com>
2011-05-01 14:41:21 +0200

    create the MockCA project, an easy solution for test certificates management

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MockCA::Application.config.secret_token = '0cce2121695abea03851384d3576eba7bb40f45662794853a3e944bb0f917a0172c4291f315fd4166dc7a28d4bccef92a6fdf60948ea6a08103402414edccae3'
